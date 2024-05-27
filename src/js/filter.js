c3cd2ab8e3e32cd260cc3f86a21283f78eacec7e
nurbek2107 <abduaxatov002@gmail.com>
2024-05-27 14:39:02 +0500

    main
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,16 +1,16 @@
 const searchFormEl = document.querySelector('.search')
 
 searchFormEl.search.addEventListener("input", () => {
-   const searchValue = searchFormEl.search.value.toLowerCase()
-   const cardsItem = document.querySelectorAll('.cards__item')
-   const cardsTitles = document.querySelectorAll('.cards__title')
-   cardsTitles.forEach((title, i) => {
+    const searchValue = searchFormEl.search.value.toLowerCase()
+    const cardsItem = document.querySelectorAll('.cards__item')
+    const cardsTitles = document.querySelectorAll('.cards__title')
+    cardsTitles.forEach((title, i) => {
     if (title.textContent.toLowerCase().includes(searchValue)) {
         cardsItem[i].style.display = 'block'
     } else {
         cardsItem[i].style.display = 'none'
     }
-   })
+})
 })
 
 const searchSelect = document.querySelectorAll('.search__select-list li')
